75ebd626da18cf10f7295af6a1dc991d762b6496
frmdstryr <frmdstryr@protonmail.com>
2020-10-26 10:50:01 -0400

    Add underscore_to_space string filter

--- src/oscar/templatetags/string_filters.py
+++ src/oscar/templatetags/string_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def split(value, separator=' '):
     return value.split(separator)
+
+
+@register.filter
+def underscore_to_space(value):
+    return value.replace("_", " ")
